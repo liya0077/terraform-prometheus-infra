d06d96231178442a37c3f71d43ade59366ae1aae
Liyakath Ali <129610530+liya0077@users.noreply.github.com>
2025-08-18 18:36:05 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
   description = "VPC ID"
   value       = aws_vpc.main.id
 }
+
+output "alb_dns_name" {
+  description = "DNS name of the Prometheus ALB"
+  value       = aws_lb.prometheus_alb.dns_name
+}
